a983cbace69ce3d1d7785a4974069cc8c5b264c2
yurii2007 <yurash862@gmail.com>
2024-08-17 11:48:31 +0300

    added thread pool structure

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
-use std::{ net::{ TcpListener, TcpStream }, io::{ prelude::*, BufReader }, fs };
+use std::{
+    net::{ TcpListener, TcpStream },
+    io::{ prelude::*, BufReader },
+    fs,
+    thread,
+    time::Duration,
+};
+
+use rust_web_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:60").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| { handle_connection(stream) });
     }
 }
 
@@ -18,10 +27,13 @@ fn handle_connection(mut stream: TcpStream) {
     //     .take_while(|line| !line.is_empty())
     //     .collect();
     let request_line = buf_reader.lines().next().unwrap().unwrap();
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "index.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "index.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
     let content = fs::read_to_string(filename).unwrap();
